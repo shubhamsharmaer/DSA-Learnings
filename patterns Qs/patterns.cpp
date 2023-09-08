cea0d22d99752468fa4657b7e7b89e810a714532
Shubham Sharma <shubhamgetmail@gmail.com>
2023-09-08 18:56:44 +0530

    done upto hollow full pyramid

--- patterns Qs/patterns.cpp
+++ patterns Qs/patterns.cpp
@@ -67,12 +67,17 @@ int main() {
 // hollow full pyramid 
   for(int row=0; row<n; row++){
     // for outer spaces
-    for(int col=0; col<n-row-1; col++){
+    int k=0;
+    for(int col=0; col<(2*n-1); col++){
+      if(col<n-row-1)
+      {
       cout<<" ";
-    }
-    for(int col=0; col<row+1; col++){
-      if(col==0 || col==col-n || row==n-1){
-        cout<<"* ";
+      }else if(k<2*row+1){
+        if(k==0 || k==2*row || row==n-1)
+        cout<<"*";
+        else
+          cout<<" ";
+        k++;  
       }else{
         cout<<" ";
       }
