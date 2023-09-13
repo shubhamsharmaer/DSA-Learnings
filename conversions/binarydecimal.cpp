f92a903b1bb0607fcde271a587f541471b725135
Shubham Sharma <shubhamgetmail@gmail.com>
2023-09-13 16:22:02 +0530

    Bitwise right shift method added

--- conversions/binarydecimal.cpp
+++ conversions/binarydecimal.cpp
@@ -1,28 +1,40 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
-int decimaltobinary(int n){
+int decimaltobinaryM1(int n)
+{
     // division method
-    int i=0;
-    int binaryno=0;
-    while(n>0){
-        int bit = n%2;
-        binaryno = bit*pow(10, i++) + binaryno;
-        n = n/2;
+    int i = 0;
+    int binaryno = 0;
+    while (n > 0)
+    {
+        int bit = n % 2;
+        binaryno = bit * pow(10, i++) + binaryno;
+        n = n / 2;
     }
     return binaryno;
 }
 
-int decimaltobinary2(int n){
-    
+int decimaltobinaryM2(int n)
+{
+    // bitwise AND method
+    int binaryno = 0;
+    int i = 0;
+    while (n > 0)
+    {
+        int bit = (n & 1);
+        binaryno = bit * pow(10, i++) + binaryno;
+        n = n >> 1;
+    }
+    return binaryno;
 }
 
-// TODO: add decimaltobinary() method 2
-
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    int binaryconverted = decimaltobinary(n);
-    cout<<binaryconverted;
+    cin >> n;
+    // int binaryconverted = decimaltobinaryM1(n);
+    // int binaryconverted = decimaltobinaryM2(n);
+    cout << binaryconverted;
 }
