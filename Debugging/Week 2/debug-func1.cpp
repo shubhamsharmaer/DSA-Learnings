271fa283d63a4bc531515c68a673bf0aba4b30be
Shubham Sharma <shubhamgetmail@gmail.com>
2023-09-13 15:29:19 +0530

    functions debugging added

---
+++ Debugging/Week 2/debug-func1.cpp
@@ -0,0 +1,14 @@
+// NOTES: missing <cmath> library
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double width, height;
+    cin >> width >> height;
+    int area = width * height;
+    cout << area << endl;
+    return 0;
+}
