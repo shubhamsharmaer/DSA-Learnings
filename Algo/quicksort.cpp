3060d0bc195896412092472f2c3181d9cd3f3415
Shubham Sharma <shubhamgetmail@gmail.com>
2023-10-23 22:28:26 +0530

    added quick sort algo

---
+++ Algo/quicksort.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+// main logic
+// void quicklogic(int arr[], int i, int j, int pivot){
+//     pivot = arr.size()-1;
+
+// }
+
+// quick sort by recursion
+void quicksort(int arr[], int start, int end)
+{
+
+    // base case
+    if (start >= end)
+    {
+        return;
+    }
+
+    int i = start - 1;
+    int j = start;
+    int pivot = end;
+
+    // loop for comparing and swaping elements
+    while (j < pivot)
+    {
+        if (arr[j] < arr[pivot])
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+        j++;
+    }
+    i++;
+    swap(arr[i], arr[pivot]);
+
+    // break arr
+    quicksort(arr, start, i - 1);
+    quicksort(arr, i + 1, end);
+
+    // call logic function
+    // quicklogic(arr, i, j, pivot);
+}
+
+int main()
+{
+    int n = 8;
+    int arr[n] = {2, 4, 1, 5, 7, 3, 9, 10};
+    int start = 0;
+    int end = n - 1;
+    cout << "Before sorting:";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    quicksort(arr, start, end);
+    cout << "After sorting:";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
