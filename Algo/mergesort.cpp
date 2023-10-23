9b136ca61ad94e0a70b6f16766ecb00388617764
Shubham Sharma <shubhamgetmail@gmail.com>
2023-10-23 21:43:38 +0530

    added merge sort algo

---
+++ Algo/mergesort.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// merge logic funtion
+void mergelogic(int nums[], int s, int e)
+{
+
+    int mid = (s + e) / 2;
+    // define length of
+    // left arr
+    int leftlen = mid - s + 1;
+    // right arr
+    int rightlen = e - mid;
+
+    // create left arr
+    int leftarr[leftlen];
+    // create right arr
+    int rightarr[rightlen];
+
+    // copy left arr using for loop & k variable
+    int k = s;
+    for (int i = 0; i < leftlen; i++)
+    {
+        leftarr[i] = nums[k];
+        k++;
+    }
+    // copy right arr using for loop & k variable
+    k = mid + 1;
+    for (int i = 0; i < rightlen; i++)
+    {
+        rightarr[i] = nums[k];
+        k++;
+    }
+
+    // define indexes for
+    // left arr
+    int leftindx = 0;
+    // right arr
+    int rightindx = 0;
+    // mid position
+    int midindx = s;
+
+    // while loop for comparing both arr's element and         merge into original arr
+    while (leftindx < leftlen && rightindx < rightlen)
+    {
+        if (leftarr[leftindx] < rightarr[rightindx])
+        {
+            nums[midindx] = leftarr[leftindx];
+            midindx++;
+            leftindx++;
+        }
+        else
+        {
+            nums[midindx] = rightarr[rightindx];
+            midindx++;
+            rightindx++;
+        }
+    }
+}
+
+// merge by recursion
+void merge(int nums[], int s, int e)
+{
+    // base case
+    if (s >= e)
+        return;
+    int mid = (s + e) / 2;
+
+    // break arry
+    merge(nums, s, mid);
+    merge(nums, mid + 1, e);
+
+    // recursive call
+    mergelogic(nums, s, e);
+}
+
+int main()
+{
+    int size = 10;
+    int nums[size] = {23, 45, 23, 13, 5, 6, 23, 78, 12, 13};
+    int s = 0;
+    int e = size - 1;
+    cout << "Before sorting: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    merge(nums, s, e);
+    cout << "After sorting: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << nums[i] << " ";
+    }
+}
