2913f6c220b01e5900515f86b8fbf163c6006fb9
Shubham Sharma <shubhamgetmail@gmail.com>
2023-12-24 14:14:27 +0530

    Update -> added print queue logic

--- Practice Daily Concepts/Queue/reverseKelement.cpp
+++ Practice Daily Concepts/Queue/reverseKelement.cpp
@@ -22,43 +22,14 @@ void printqueue(queue<int> q)
 void reversequeue(queue<int> &q, int k)
 {
     // STEP 1: Make stack
-    stack<int> st;
-    int n = q.size();
 
     // STEP 2: Pop K element from queue and push -> stack
-    for (int i = 0; i < k; i++)
-    {
-        int temp = q.front();
-        q.pop();
-        st.push(temp);
-    }
+
     // STEP 3: Push back popped elements into queue
-    while (!st.empty())
-    {
-        int temp = st.top();
-        st.pop();
-        q.push(temp);
-    }
+
     // STEP 4: Pop left (n-k) elements and push back in queue
-    for (int i = 0; i < (n - k); i++)
-    {
-        int temp = q.front();
-        q.pop();
-        q.push(temp);
-    }
 }
 
 int main()
 {
-    queue<int> q;
-    q.push(10);
-    q.push(20);
-    q.push(30);
-    q.push(40);
-    q.push(50);
-    q.push(60);
-
-    printqueue(q);
-    reversequeue(q, 4);
-    printqueue(q);
 }
