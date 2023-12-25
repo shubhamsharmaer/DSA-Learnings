eed0fbb404587749b89679b6664b52198a75c595
Shubham Sharma <shubhamgetmail@gmail.com>
2023-12-25 09:44:32 +0530

    Update -> Queue Implementation, Test Print() function

--- Practice Daily Concepts/Queue/interleavequeue.cpp
+++ Practice Daily Concepts/Queue/interleavequeue.cpp
@@ -25,4 +25,13 @@ void interleavequeue(queue<int> &first)
 
 int main()
 {
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+    cout << "Queue: ";
+    print(q);
 }
