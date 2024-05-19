c6729c56fbcc37285f99ed89754dff063f24e46a
Shubham Sharma <shubhamgetmail@gmail.com>
2024-05-19 21:32:03 +0530

    M3 Applied

--- Basic Maths/primecountmethods.cpp
+++ Basic Maths/primecountmethods.cpp
@@ -36,9 +36,30 @@ int countprime(int n){
     return c;
 }
 
+// NOTES: M3 Sieve of Eratosthenes
+int countprimes(int n){
+    if(n == 0) return 0;
+    int ans = 0;
+    // make a vector arr
+    vector<bool>prime(n, true);
+    prime[0] = prime[1] = false;
 
+    for(int i = 2; i< n; i++){
+        if(prime[i]){
+            ans++;
+            
+            int j = 2*i;
+            while(j < n){
+             prime[j] = false;
+             j += i;
+            }
+
+        }
+    }
+    return ans;
+}
 
 int main(){
-    int ans = countprime(100);
+    int ans = countprimes(100);
     cout<<ans;
 }
